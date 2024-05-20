43d745f0ab92231102412300e1b7acae31bd2baf
akmal09 <akmalfauzan.af@gmail.com>
2024-05-20 20:02:03 +0700

    Initial commit

---
+++ src/main/java/com/project/main/MainApplication.java
@@ -0,0 +1,14 @@
+package com.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
